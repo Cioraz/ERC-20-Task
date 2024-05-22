65e2dac4bb84d7c7481344b7b98f1ff313a7cbfb
Cioraz <sunilthunga.221cs252@nitk.edu.in>
2024-05-22 23:00:13 +0530

    Created Tokens A and B

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -8,4 +8,27 @@ contract SimpleDEX{
     IERC20 public tokenB;
     uint public exchangeRate;
 
+    modifier onlyOwner(){
+        require(msg.sender==owner,"Error: Not Authorised!");
+        _;
+    }
+
+    constructor(address _tokenA,address _tokenB, uint _exchangeRate){
+        owner=msg.sender;
+        tokenA=IERC20(_tokenA);
+        tokenB=IERC20(_tokenB);
+        exchangeRate=_exchangeRate;
+    }
+
+    function setExchangeRate(uint _newRate) public onlyOwner {
+        exchangeRate=_newRate;
+    }
+
+    function exchangeTokenAForTokenB(uint amountA) public  {
+        // Ensure contract has enough tokenB for transfer
+        require(tokenB.balanceOf(address(this))>=amountA*exchangeRate,"Insufficient Tokens B");
+        // Transfer tokenA from user to DEX contract
+        require(tokenA.transferFrom(msg.sender,address(this),amountA),"Error: Transfer to DEX failed!");
+        tokenB.transfer(msg.sender,amountA*exchangeRate);
+    }
 }
